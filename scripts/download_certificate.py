6c78ea9cb7a0811ca1a1c481c5011ccc15d95f58
Kirill Logachev <logachev.k@gmail.com>
2018-03-22 09:30:44 -0400

    Implement MSI auth (#9)

--- scripts/download_certificate.py
+++ scripts/download_certificate.py
@@ -4,23 +4,31 @@ from OpenSSLWrapper import OpenSSLWrapper
 
 from OpenSSL import crypto
 import os
+import time
 
 hostname = os.uname()[1]
 
 cfg = Config()
 openssl = OpenSSLWrapper()
 
-kvwrapper = KeyVaultWrapper(
-    client_id = cfg.client_id, 
-    secret = cfg.secret,
-    tenant_id = cfg.tenant_id)
+def load_data():
+    kvwrapper = KeyVaultWrapper()
 
-kvwrapper.get_secret_to_file(cfg.vault_uri, 'root-ca', '', 'root_ca.pem')
-kvwrapper.get_secret_to_file(cfg.vault_uri, 'intermediate-ca', '', 'intermediate_ca.pem')
+    kvwrapper.get_secret_to_file(cfg.vault_uri, 'root-ca', '', 'root_ca.pem')
+    kvwrapper.get_secret_to_file(cfg.vault_uri, 'intermediate-ca', '', 'intermediate_ca.pem')
 
-kvwrapper.get_certificate_and_key_to_file(
-    vault_uri = cfg.vault_uri,
-    cert_name = hostname,
-    cert_version = '',
-    key_filename = 'new.key',
-    cert_filename = 'new.pem')
+    kvwrapper.get_certificate_and_key_to_file(
+        vault_uri = cfg.vault_uri,
+        cert_name = hostname,
+        cert_version = '',
+        key_filename = 'new.key',
+        cert_filename = 'new.pem')
+
+while True:
+    try:
+        load_data()
+    except:
+        time.sleep(5)
+        continue
+    else:
+        break
