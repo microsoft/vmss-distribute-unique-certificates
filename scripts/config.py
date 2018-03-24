da4772fc97a0568c9edc752db0a78fb5dbf24871
Jeff Day <jeffday@microsoft.com>
2018-03-24 03:18:08 -0700

    Added comment.

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,8 @@
 class Config(object):
     def __init__(self):
+        # Use the values from the subscription hosting your KeyVault instance.
         self.client_id = ""
         self.secret = ""
-        self.tenant_id = ''
+        self.tenant_id = ""
         
         self.vault_uri = ""
