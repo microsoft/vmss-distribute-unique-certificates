236838b72d98161b63fa9a04c75c34b2696ede76
Jeff Day <jeffday@microsoft.com>
2018-03-23 18:56:46 -0700

    Added support for KV in a remote subscription; added comments; refactored naming; output SPN at end

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,54 @@
+# Kay Vault Variables
+variable "key_vault_name" {
+  default = ""
+}
+
+variable "key_vault_subscription_id" {
+  default = ""
+}
+
+# Use this same value for the client ID in scripts/configy.py
+# Add this application to the Key Vault subscription IAM with permission to create resources
+variable "key_vault_client_id" {
+  description = "Application ID value from an Active Directory App Registration."
+  default = ""
+}
+
+# Use this same value for the secret ID in scripts/configy.py
+variable "key_vault_client_secret" {
+  description = "Secret value obtained by creating a key for the App Registration."
+  default = ""
+}
+
+variable "key_vault_tenant_id" {
+  description = "Active Directory 'Directory ID' property."
+  default = ""
+}
+
+variable "key_vault_resource_group_name" {
+  description = "The name of the resource group in which to create the virtual network."
+  default = ""
+}
+
+variable "key_vault_location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default     = ""
+}
+
+# Scale Set Variables
 variable "subscription_id" {
   default = ""
 }
 
+# Add this application to the Key Vault subscription IAM with permission to create resources
 variable "client_id" {
+  description = "Application ID value from an Active Directory App Registration."
   default = ""
 }
 
 variable "client_secret" {
-  default = ""
+  description = "Secret value obtained by creating a key for the App Registration."
+  default = "GMRZ8IZFqnX00wVUsk9hoiGRirLF/sm9U+GiYhsu4YY="
 }
 
 variable "tenant_id" {
@@ -17,11 +58,12 @@ variable "tenant_id" {
 
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the virtual network."
+  default = ""
 }
 
 variable "location" {
   description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
-  default     = "southcentralus"
+  default     = ""
 }
 
 variable "storage_account_tier" {
@@ -34,7 +76,7 @@ variable "storage_replication_type" {
   default     = "LRS"
 }
 
-variable "hostname" {
+variable "vmss_prefix" {
   default = ""
 }
 
@@ -56,10 +98,6 @@ variable "image_offer" {
   default     = "CentOS"
 }
 
-variable "vmss_name_prefix" {
-  default = ""
-}
-
 variable "instance_count" {
   description = "Number of VM instances (100 or less)."
   default     = "5"
@@ -71,4 +109,16 @@ variable "admin_username" {
 
 variable "admin_password" {
   default = ""
+}
+
+variable "command" {
+  default = "bash bootstrap.sh"
+}
+
+variable "file1" {
+  default = "https://baccertificates.blob.core.windows.net/vm-scripts-files/bootstrap.sh"
+}
+
+variable "file2" {
+  default = "https://baccertificates.blob.core.windows.net/vm-scripts-files/vm_scripts.tar.gz"
 }
